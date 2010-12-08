87fca534cd9412d268f4f9346e26e4af1496099f
Gregor Martynus <gregor@martynus.net>
2010-12-08 20:07:42 +0100

    added support for custom domains

--- server.js
+++ server.js
@@ -4,11 +4,21 @@ var http = require('http'),
       'Access-Control-Allow-Methods' : 'POST, GET, PUT, DELETE, OPTIONS',
       'Access-Control-Max-Age'       : '86400', // 24 hours
       'Access-Control-Allow-Headers' : 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
-    };
+    },
+    passed_domain = false;
 
 // add passed extra headers
 for (var i=2; i < process.argv.length; i++) {
-  cors_headers['Access-Control-Allow-Headers'] += ', ' + process.argv[i];
+  switch (true) {
+    case /^\X-/.test(process.argv[i]):
+      cors_headers['Access-Control-Allow-Headers'] += ', ' + process.argv[i];
+      break;
+    case /[\w.]+(:\d+)?/.test(process.argv[i]):
+      passed_domain = process.argv[i];
+      break;
+    default:
+      console.log('Dunno what to do with param: '+process.argv[i]) 
+  }
 };
 
 http.createServer(function(request, response) {
@@ -19,14 +29,18 @@ http.createServer(function(request, response) {
     return;
   }
   
-  request.headers['host']  = request.headers['host']
-                             // remove port
-                             .replace(/\:.*$/,'')
-                             // proxy.example.com => example.com
-                             .replace(/proxy\./,'');
+  request.headers['host']  = (passed_domain || request.headers['host'])
+                              // remove port
+                              .replace(/\:.*$/,'')
+                              // proxy.example.com => example.com
+                              .replace(/proxy\./,'');
+  
+  console.log(request.headers['host'], request.method, request.url);
+  
   var proxy = http.createClient(80, request.headers['host']);
-  console.log(request.method, request.headers['host'] + request.url);
   var proxy_request = proxy.request(request.method, request.url, request.headers);
+  
+  
   proxy_request.addListener('response', function (proxy_response) {
     proxy_response.addListener('data', function(chunk) {
       response.write(chunk, 'binary');
@@ -46,4 +60,4 @@ http.createServer(function(request, response) {
   request.addListener('end', function() {
     proxy_request.end();
   });
-}).listen(process.env.PORT || 8080);
+}).listen(process.env.PORT || 1234);
