ecd99c168e69422966eef2d22e29ff1ae31943dc
Gregor Martynus <gregor@martynus.net>
2015-08-17 15:02:00 -0400

    test: check for request results

--- test/test-debug-page.js
+++ test/test-debug-page.js
@@ -12,6 +12,20 @@ describe('example app', function () {
   it('Heading is "CORS Proxy test"', function () {
     return this.browser
       .elementByCssSelector('h1').text()
-      .should.eventually.equal('CORS Proxy test')
+        .should.eventually.equal('CORS Proxy test')
+  })
+
+  it('Example 1 result is Error', function () {
+    return this.browser
+      .waitForConditionInBrowser('!! document.querySelector("#example-1 .result").textContent', 10000)
+      .elementByCssSelector('#example-1 .result').text()
+        .should.eventually.equal('Error')
+  })
+
+  it('Example 2 result is Error', function () {
+    return this.browser
+      .waitForConditionInBrowser('!! document.querySelector("#example-2 .result").textContent', 10000)
+      .elementByCssSelector('#example-2 .result').text()
+        .should.eventually.equal('Success')
   })
 })
